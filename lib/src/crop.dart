3512039d96b4a5cc8b26a018ad4f979dacf0c129
Luke Hutchison <luke.hutch@gmail.com>
2023-02-28 22:17:41 -0700

    Revert prev commit

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -74,7 +74,7 @@ class Crop extends StatelessWidget {
   /// [CornerDotBuilder] passes [size] which indicates the size of each dots
   /// and [EdgeAlignment] which indicates the position of each dots.
   /// If default dot Widget with different color is needed, [DotControl] is available.
-  final Widget? cornerDotBuilder;
+  final CornerDotBuilder? cornerDotBuilder;
 
   /// If [true], cropping area is fixed and CANNOT be moved.
   /// [false] by default.
